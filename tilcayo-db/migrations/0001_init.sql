c38952f549e548ea4de57fdfa595cbe0e37798e1
VadimStorozhilov <yanechearer@gmail.com>
2026-09-22 18:08:13 +0500

    feat: initial tilcayo db

---
+++ tilcayo-db/migrations/0001_init.sql
@@ -0,0 +1,28 @@
+CREATE TABLE roles (
+    id BIGSERIAL PRIMARY KEY
+);
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    role_id BIGINT NOT NULL REFERENCES roles(id)
+);
+
+CREATE TABLE schemas (
+    id BIGSERIAL PRIMARY KEY,
+    schema JSONB NOT NULL
+);
+
+CREATE TABLE resources (
+    id BIGSERIAL PRIMARY KEY,
+    schema_id BIGINT NOT NULL REFERENCES schemas(id),
+    data JSONB NOT NULL
+);
+
+CREATE TABLE permissions (
+    role_id BIGINT NOT NULL REFERENCES roles(id),
+    schema_id BIGINT NOT NULL REFERENCES schemas(id),
+    read BOOLEAN NOT NULL DEFAULT FALSE,
+    write BOOLEAN NOT NULL DEFAULT FALSE,
+
+    PRIMARY KEY (role_id, schema_id)
+);
